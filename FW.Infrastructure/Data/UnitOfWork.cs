1dd6d296385b01854c3032d97d4e7366dd09f323
Mohamed <mo7amed.1.khalifa@gmail.com>
2026-06-08 16:50:29 +0300

    Add Unit Of Wrok implementaion and Transaction logic

---
+++ FW.Infrastructure/Data/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using FW.Domain;
+using Microsoft.EntityFrameworkCore.Storage;
+namespace FW.Infrastructure;
+
+public class UnitOfWork(AppDbContext db) : IUnitOfWork
+{
+    public async Task ExecuteInTransactionAsync(Func<Task> action, CancellationToken ct = default)
+    {
+        await using IDbContextTransaction tx = await db.Database.BeginTransactionAsync(ct);
+        try
+        {
+            await action();
+            await tx.CommitAsync(ct);
+        }
+        catch
+        {
+            await tx.RollbackAsync(ct);
+            throw;
+        }
+    }
+
+    public async Task<int> SaveChangesAsync(CancellationToken ct = default)
+    {
+        return await db.SaveChangesAsync(ct);
+
+    }
+}
